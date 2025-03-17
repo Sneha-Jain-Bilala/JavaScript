56b744c21ac274449245af317e4fff72fdf372f3
Sneha-Jain-Bilala <bilalasneha@gmail.com>
2025-03-17 13:55:17 +0000

    let and datatype

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,19 @@
 console.log("sneha")
+// console.log(); 
+let a=5;
+let b;
+
+console.log(a);
+console.table([a,b]);
+let c="sneha"
+let age=8
+let d=""
+console.log(typeof c)
+// console.log(typeof (c));  //as a method bhi pass krr skte
+ console.log(d);
+
+
+
+
+
+
