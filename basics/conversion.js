ee56b202b2747554a3d372d86b16f995260dc6cf
Sneha-Jain-Bilala <bilalasneha@gmail.com>
2025-03-23 17:59:46 +0000

    tyoeof

---
+++ basics/conversion.js
@@ -0,0 +1,35 @@
+// let score ="33"
+// console.log(typeof score);
+
+// let valueInNo= Number(score)
+// console.log(typeof valueInNo);
+
+// 2nd part
+// let score ="33abc"
+// console.log(typeof score);
+
+// let valueInNo= Number(score)
+// console.log(valueInNo);
+// console.log(typeof valueInNo);
+
+// 3rd part
+// let score=null
+// console.log(typeof score);
+
+// let valueInNo= Number(score)
+// console.log(valueInNo);
+// console.log(typeof valueInNo);
+
+// 4th part
+
+let score =undefined
+console.log(typeof score);
+
+let valueInNo= Number(score)
+console.log(valueInNo);
+console.log(typeof valueInNo);
+
+
+
+
+
